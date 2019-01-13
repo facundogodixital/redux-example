f8eef09ed8dfe7df18da105f2d0d95e27dbed411
Facundo Fasciolo <facundo@godixital.com>
2019-01-13 19:10:06 -0300

    Implementacion con React-Redux. Conectado componente ShoppingCart. (Mantiene logica anterior de redux nativo comentada y comentarios).

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Panel, Table, Button, Glyphicon } from 'react-bootstrap';
-import store from '../store';
-import {getRemoveFromCartAction} from '../actionCreators';
+import { getRemoveFromCartAction } from '../actionCreators';
+import { connect } from 'react-redux';
 
 
 const styles = {
@@ -11,7 +11,15 @@ const styles = {
 }
 
 
+
+// Podría ya, al haber implementado react-redux, dejar este componente como una función (solo presentacional).
+// Ej: const ShoppingCart = (props) => { ... };
+
 class ShoppingCart extends Component {
+
+
+  // En este caso, al usar react-redux, no necesito implementar el constructor.
+  /*
   constructor() {
     super();
     this.removeFromCart = this.removeFromCart.bind(this);
@@ -33,24 +41,33 @@ class ShoppingCart extends Component {
 
     console.log('ShoppingCart::constructor | Subscribe realizado.');
   }
+  */
+
 
   render() {
     return (
       <Panel header="Shopping Cart">
         <Table fill>
           <tbody>
-            {this.state.cart.map(product =>
+            {/* 
+              Ya NO uso el state del componente (this.state.cart), ya que react-redux inyecta el cart mediante props.
+              (Usando el método mapStateToProps, tomo del state de redux los elementos que necesita el componente y 
+              se los inyecta de forma automática a través de props).
+            */}
+            {this.props.cart.map(product =>
               <tr key={product.id}>
                 <td>{product.name}</td>
                 <td className="text-right">${product.price}</td>
-                <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => this.removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
+
+                {/* El método removeFromCart es inyectado a través de props por la función mapDispatchToProps del connect */}
+                <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => this.props.removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
               </tr>
             )}
           </tbody>
           <tfoot>
             <tr>
               <td colSpan="4" style={styles.footer}>
-                Total: ${this.state.cart.reduce((sum, product) => sum + product.price, 0)}
+                Total: ${this.props.cart.reduce((sum, product) => sum + product.price, 0)}
               </td>
             </tr>
           </tfoot>
@@ -61,11 +78,39 @@ class ShoppingCart extends Component {
   }
 
 
+  //Este método también lo quito, ya que se lo paso mediante props usando la funcion "mapDispatchToProps" de react-redux.
+  /*
   removeFromCart(product) {
     store.dispatch(getRemoveFromCartAction(product));
   }
+  */
 
 
 }
 
-export default ShoppingCart;
+// Indica cómo transformar el state actual del store Redux en los props que desea pasar a un componente de presentación.
+// En este caso, el componente ShoppingCart necesita recibir solamente el objeto "cart" del state como prop.
+// La función recibe como parámetro de forma automática el store.state (parametro: "state")
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
+  };
+};
+
+
+// Le inyecta al componente mediante props, los métodos que necesita.
+// La función recibe como parámetro de forma automática el método store.dispatch (parametro: "dispatch")
+const mapDispatchToProps = dispatch => {
+  return {
+    removeFromCart: product => {
+      dispatch(getRemoveFromCartAction(product));
+    }
+  };
+};
+
+
+// Conecto el componente con React-Redux.
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(ShoppingCart);
