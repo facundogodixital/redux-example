f52ff7e7c3b02753161598dd105234c1569f9d82
Facundo Fasciolo <facundo@godixital.com>
2019-01-13 19:17:52 -0300

    Implementacion con React-Redux. Conectado componente ShoppingCart. (Quitados comentarios de codigo anterior, version mas limpia). Usando presentational component.

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Panel, Table, Button, Glyphicon } from 'react-bootstrap';
 import { getRemoveFromCartAction } from '../actionCreators';
 import { connect } from 'react-redux';
@@ -11,82 +11,36 @@ const styles = {
 }
 
 
+// Hago destructuring en lugar de hacer " const ShoppingCart = (props) "
+const ShoppingCart = ({cart, removeFromCart}) => {
+  return (
+    <Panel header="Shopping Cart">
+      <Table fill>
+        <tbody>
+          {cart.map(product =>
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td className="text-right">${product.price}</td>
 
-// Podría ya, al haber implementado react-redux, dejar este componente como una función (solo presentacional).
-// Ej: const ShoppingCart = (props) => { ... };
-
-class ShoppingCart extends Component {
-
-
-  // En este caso, al usar react-redux, no necesito implementar el constructor.
-  /*
-  constructor() {
-    super();
-    this.removeFromCart = this.removeFromCart.bind(this);
-
-    this.state = {
-      cart: []
-    }
-
-
-    // Me subscribo al store, para que re-renderice el carrito de compras cuando haya un cambio de state.
-    // Ese cambio de state, lo va a hacer el reducer, si se dispatchea la acción "ADD_TO_CART".
-    store.subscribe(() => {
-      this.setState({
-        cart: store.getState().cart
-      });
-
-      console.log('Subscribe llamado | state actual: ', store.getState());
-    });
-
-    console.log('ShoppingCart::constructor | Subscribe realizado.');
-  }
-  */
-
-
-  render() {
-    return (
-      <Panel header="Shopping Cart">
-        <Table fill>
-          <tbody>
-            {/* 
-              Ya NO uso el state del componente (this.state.cart), ya que react-redux inyecta el cart mediante props.
-              (Usando el método mapStateToProps, tomo del state de redux los elementos que necesita el componente y 
-              se los inyecta de forma automática a través de props).
-            */}
-            {this.props.cart.map(product =>
-              <tr key={product.id}>
-                <td>{product.name}</td>
-                <td className="text-right">${product.price}</td>
-
-                {/* El método removeFromCart es inyectado a través de props por la función mapDispatchToProps del connect */}
-                <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => this.props.removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
-              </tr>
-            )}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="4" style={styles.footer}>
-                Total: ${this.props.cart.reduce((sum, product) => sum + product.price, 0)}
-              </td>
+              {/* El método removeFromCart es inyectado a través de props por la función mapDispatchToProps del connect */}
+              <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
             </tr>
-          </tfoot>
-        </Table>
+          )}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="4" style={styles.footer}>
+              Total: ${cart.reduce((sum, product) => sum + product.price, 0)}
+            </td>
+          </tr>
+        </tfoot>
+      </Table>
 
-      </Panel>
-    )
-  }
-
-
-  //Este método también lo quito, ya que se lo paso mediante props usando la funcion "mapDispatchToProps" de react-redux.
-  /*
-  removeFromCart(product) {
-    store.dispatch(getRemoveFromCartAction(product));
-  }
-  */
-
-
+    </Panel>
+  )
 }
+
+
 
 // Indica cómo transformar el state actual del store Redux en los props que desea pasar a un componente de presentación.
 // En este caso, el componente ShoppingCart necesita recibir solamente el objeto "cart" del state como prop.
